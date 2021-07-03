aec1d12d2df12e9a7ea212643f6a8ec3d56d1fb0
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-03 14:23:36 +0700

    Added 2 function-based views. One =  post list view. 2 = post detail view that handles get, put and delete requests.

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY_CHOICES = (
+    ('Dj', 'Django'),
+    ('R', 'Ruby')
+)
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    custom_id = models.IntegerField()
+    category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
